0968dfd95cdf147db0752a6f80f692597d5eb3ff
Ahmed Emad <emada2826@gmail.com>
2025-08-17 10:48:50 +0300

    completing crud of student

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,12 @@
 package com.example.demo.student;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
+
 @Service // ✅ This registers it as a Spring bean
 public class StudentService {
     private final StudentRepository studentRepository;
@@ -14,4 +17,36 @@ public class StudentService {
     public List<Student> getStudents() {
        return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentByEmail.isPresent())
+        {
+           throw new IllegalStateException("email is taken");
+        }
+        else {
+            studentRepository.save(student);
+            System.out.println(
+                    student+ " saved successfully"
+            );
+        }
+    }
+    @Transactional
+    public void updateStudent(Long studentId,String name , String email)
+    { Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("Student doesn't exist"));;
+
+        student.setName(name);
+        student.setEmail(email);
+
+
+    }
+
+    public void deleteStudent(long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists) {
+        throw new IllegalStateException("Student doesn't exist");
+        }
+    studentRepository.deleteById(studentId);
+    }
+
 }
